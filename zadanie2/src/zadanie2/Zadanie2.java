ca338cab2042118b9225d420a42c0c267cb674be
Paweł Szrubas <pawel050673@gmail.com>
2018-10-17 20:11:36 +0200

    pierwszy

---
+++ zadanie2/src/zadanie2/Zadanie2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zadanie2;
+
+/**
+ *
+ * @author pawel
+ */
+public class Zadanie2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int liczba =5;
+        int[] tablica = new int [liczba];
+        
+    }
+    
+}
