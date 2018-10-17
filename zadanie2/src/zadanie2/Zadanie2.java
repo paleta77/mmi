971c0b9e5279511395e85602f75e330fe75f5507
Paweł Szrubas <pawel050673@gmail.com>
2018-10-17 20:51:41 +0200

    wczytuje i wypisuje

--- zadanie2/src/zadanie2/Zadanie2.java
+++ zadanie2/src/zadanie2/Zadanie2.java
@@ -5,6 +5,8 @@
  */
 package zadanie2;
 
+import java.util.Scanner; //do wczytawania z klawiatury
+
 /**
  *
  * @author pawel
@@ -14,11 +16,44 @@ public class Zadanie2 {
     /**
      * @param args the command line arguments
      */
+    public static int[][] wczytaj_figure() {  
+        System.out.print("Podaj ilosc wierzcholkow: ");
+        Scanner scan = new Scanner(System.in); //do wczytawania z klawiatury
+        
+        int wielkosc; //ilosc wierzcholkow
+        wielkosc = scan.nextInt();
+        
+        int tablica[][] = new int[wielkosc][2];
+        
+        for (int i = 0; i<wielkosc; i++)
+        {
+            System.out.println("wierzcholek: " + (i+1));
+            System.out.println("x: ");
+            tablica[i][0]=scan.nextInt();
+            System.out.print("y: ");
+            tablica[i][1]=scan.nextInt();
+        }
+        
+        
+        scan.close(); //zakonczenie wczytawania z klawiatury
+        return tablica; 
+}
+    
+    public static int[][] wypisz_tablice(int tablica[][]) {
+        for(int i=0;i<tablica.length;i++)
+        {
+            System.out.println("Wierzcholek "+i+" x:"+tablica[i][0]);
+            System.out.println("Wierzcholek "+i+" y:"+tablica[i][1]);
+            System.out.println();
+        }
+        return tablica;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        int liczba =5;
-        int[] tablica = new int [liczba];
         
+        int[][] wierzcholki = wczytaj_figure(); //tablica ze wspolrzednymi wierzcholkow
+        wypisz_tablice(wierzcholki);
     }
     
 }
