3327fb9a1b65c8c2e74c223a1a7fe23353976f22
agnieszka2018 <baga16@o2.pl>
2018-10-20 16:21:02 +0200

    początek swing

--- zadanie2/src/zadanie2/Zadanie2.java
+++ zadanie2/src/zadanie2/Zadanie2.java
@@ -1,59 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package zadanie2;
 
 import java.util.Scanner; //do wczytawania z klawiatury
+import javax.swing.*; //do interface'u graficznego
+import java.awt.*;  //do grafiki 2D
+import java.awt.geom.*;
 
-/**
- *
- * @author pawel
- */
-public class Zadanie2 {
+public class Zadanie2 extends JFrame {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static int[][] wczytaj_figure() {  
+    JButton confirm = new JButton("Zatwierdź"); //tworzenie obiektu przycisku
+    GeneralPath polygon = new GeneralPath();    //tworzenie obiektu dla wielokąta
+    
+    // konstruktor klasy Zadanie2
+    public Zadanie2() {
+        setTitle("Zadanie 2 z MMI");
+        setSize(1000, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLookAndFeel();
+        JPanel contener = new JPanel(); //tworzenie kontenera
+        contener.add(confirm);  //dodawanie przycisku do kontenera
+        add(contener);  //dodanie kontenera do ramki
+        setVisible(true);
+
+    }
+
+    //ustawienie wyglądu i działania okienka przy użyciu widoku "Windows" + wyjątek klasy Exception
+    private static void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (Exception exc) {
+        }
+    }
+
+    
+    public static int[][] wczytaj_figure() {
         System.out.println("Podaj ilosc wierzcholkow: ");
         Scanner scan = new Scanner(System.in); //do wczytawania z klawiatury
-        
+
         int wielkosc; //ilosc wierzcholkow
         wielkosc = scan.nextInt();
-        
+
         int tablica[][] = new int[wielkosc][2];
-        
-        for (int i = 0; i<wielkosc; i++)
-        {
-            System.out.println("wierzcholek: " + (i+1));
-            System.out.println("x: ");
-            tablica[i][0]=scan.nextInt();
+
+        for (int i = 0; i < wielkosc; i++) {
+            System.out.println("wierzcholek: " + (i + 1));
+            System.out.print("x: ");
+            tablica[i][0] = scan.nextInt();
             System.out.print("y: ");
-            tablica[i][1]=scan.nextInt();
+            tablica[i][1] = scan.nextInt();
         }
-        
-        
+
         scan.close(); //zakonczenie wczytawania z klawiatury
-        return tablica; 
-}
-    
+        return tablica;
+    }
+
     public static int[][] wypisz_tablice(int tablica[][]) {
-        for(int i=0;i<tablica.length;i++)
-        {
-            System.out.println("Wierzcholek "+i+" x:"+tablica[i][0]);
-            System.out.println("Wierzcholek "+i+" y:"+tablica[i][1]);
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.println("Wierzcholek " + i + " x: " + tablica[i][0]);
+            System.out.println("Wierzcholek " + i + " y: " + tablica[i][1]);
             System.out.println();
         }
         return tablica;
     }
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        int[][] wierzcholki = wczytaj_figure(); //tablica ze wspolrzednymi wierzcholkow
-        wypisz_tablice(wierzcholki);
+
+    public void paintComponent(Graphics component){
+        Graphics2D component2D = (Graphics2D) component; //rzutowanie z z klasy Graphics do klasy Graphics 2D
     }
     
+    public static void main(String[] args) {
+        setLookAndFeel();
+        Zadanie2 frame = new Zadanie2(); //utworzenie obiektu frame
+
+        int[][] wierzcholki = wczytaj_figure(); //tablica ze wspolrzednymi wierzcholkow
+        wypisz_tablice(wierzcholki);
+        // paintComponent(arg);
+
+}
 }
